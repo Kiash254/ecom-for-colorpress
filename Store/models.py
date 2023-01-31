e3835dace68b2ed4ea0d2aa8c71614ed4eb4dcd4
Kiash254 <codewithkiash@gmail.com>
2023-01-31 22:31:33 +0300

    added botton

--- Store/models.py
+++ Store/models.py
@@ -19,7 +19,7 @@ class Customer(models.Model):
     
 class Product(models.Model):
     name = models.CharField(max_length=200)
-    price = models.FloatField()
+    price = models.DecimalField(max_digits=7,decimal_places=2)
     digital = models.BooleanField(default=False, null=True, blank=False)
     image=models.ImageField(upload_to='images/',null=True,blank=True)
     image_thumbnail = ImageSpecField(source='image',
